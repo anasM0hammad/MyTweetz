b223dfc787f4de1c98defbc3fc4e19e3fd6e1fbe
anasM0hammad <anas.1633.m@gmail.com>
2019-01-01 02:17:02 +0530

    Posting the user tweets on twitter

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -18,4 +18,32 @@ class TwitterController extends Controller
      return view('twitter')->with('data' , $data);
 
     }
+
+
+    public function tweet(Request $request){
+
+       $this->validate($request,[
+           
+           'tweet' => 'required'
+       ]);
+
+       $newTweet = ['status' => $request->tweet] ;
+
+       if(!empty($request->images)){
+
+        foreach($request->images as $key => $value){
+        	$uploadMeadia = Twitter::uploadMedia(['media' => File::get($value->getRealPath())]) ;
+        	if(!empty($uploadMeadia)){
+        		$newTweet['media_ids'][$uploadMeadia->media_id_string] = $uploadMeadia->media_id_string ;
+        	}
+         }
+
+       }
+
+
+       $twitter = Twitter::postTweet($newTweet);
+
+       return back();
+
+    }
 }
